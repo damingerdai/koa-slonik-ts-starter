b94eae5cc498aad0eb34f067807aa77620a4957c
Arthur Ming <guobin.ming@pwc.com>
2023-07-31 10:38:08 +0800

    feat: use http run web server

--- server.ts
+++ server.ts
@@ -1,8 +1,31 @@
+import * as http from 'http';
 import * as dotenv from 'dotenv';
 import { app } from './src';
 
 dotenv.config();
 if (require.main === module) {
 	const port = parseInt(process.env.PORT || '3000', 10);
-	app.listen(port);
+	const server = http.createServer(app.callback());
+	const gracefulShutdown = (msg: string) => {
+		console.info(`Shutdown initiated: ${msg}`);
+		server.close(() => {
+			console.info('Shutting down...');
+			process.exit();
+		});
+	};
+	server.listen(port, '0.0.0.0', () => {
+		console.log(`Running on port: ${port}`);
+
+		// Handle kill commands
+		process.on('SIGTERM', gracefulShutdown);
+
+		// Handle interrupts
+		process.on('SIGINT', gracefulShutdown);
+
+		// Prevent dirty exit on uncaught exceptions:
+		process.on('uncaughtException', gracefulShutdown);
+
+		// Prevent dirty exit on unhandled promise rejection
+		process.on('unhandledRejection', gracefulShutdown);
+	});
 }
